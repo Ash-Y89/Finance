13699df15c4cd852c72a2f8edd41b2f1b24b9a31
Ash-Y89 <ashe.yari@gmail.com>
2025-12-06 10:52:54 -0600

    Refactoring and refining stock plotting

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -1,10 +1,18 @@
 import yfinance as yf
-
-data = yf.download("TSLA", start = '2020-01-01', end = '2020-12-31')
-
-print(data.head())
-
 from matplotlib import pyplot as plt
 
-data["High"].plot(figsize = (10,5), title = "Tesla stocks @ high price from 2020 to 2024")
-plt.show()
+# data = yf.download("TSLA", start = '2020-01-01', end = '2024-12-31')
+
+# print(data.head())
+
+# data["High"].plot(figsize = (10,5), title = "Tesla stocks @ high price from 2020 to 2024")
+# plt.show()
+
+def plot_stock(*tickers):
+    # ticker = ticker.strip().upper()
+    ticker = [t.strip().upper() for t in tickers]
+    data = yf.download(tickers, start = '2020-01-01', end = '2024-12-31')
+    data['Close'].plot(figsize = (10,5), title = f'{ticker}, Closing prices from 2020 to 2024')
+    plt.show()
+
+plot_stock('nvda', 'meta', 'aaPl')
