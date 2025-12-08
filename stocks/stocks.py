1601a362c51f5b955244a4eaf4ffcdb3de59978c
Ash-Y89 <ashe.yari@gmail.com>
2025-12-08 16:37:49 -0600

    Adding x and y labels

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -13,6 +13,9 @@ def plot_stock(*tickers):
     ticker = [t.strip().upper() for t in tickers]
     data = yf.download(tickers, start = '2020-01-01', end = '2024-12-31')
     data['Close'].plot(figsize = (10,5), title = f'{ticker}, Closing prices from 2020 to 2024')
+    plt.grid(True)
+    plt.xlabel("Year")
+    plt.ylabel("Price in USD")
     plt.show()
 
 plot_stock('nvda', 'meta', 'aaPl')
