772b9121b3fcde85111fdd1669f6337ad3c5a0d9
Ash-Y89 <ashe.yari@gmail.com>
2025-12-08 17:26:19 -0600

    Refactoring of the return calculation

--- stocks/correlation_heatmaps.py
+++ stocks/correlation_heatmaps.py
@@ -5,7 +5,7 @@ import seaborn as sns
 def corr_heatmap(*tickers):
     tickers = [t.strip().upper() for t in tickers]
     data = yf.download(tickers, start = "2020-01-01", end = "2024-12-31")
-    returns = data["Close"].pct_change()
+    returns = data[["Open", "Close"]].pct_change()
     corr = returns.corr()
     sns.heatmap(corr,annot = True, cmap= "coolwarm", linewidths=0.5)
     plt.title(f"Correlation Heatmap: {tickers}")
