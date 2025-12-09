3881ab31bd5b07c8ed251ff950437db981dba414
Ash-Y89 <ashe.yari@gmail.com>
2025-12-09 12:38:53 -0600

    Adding multiple charts

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -19,3 +19,36 @@ def plot_stock(*tickers):
     plt.show()
 
 plot_stock('nvda', 'mEta', 'aaPl')
+
+# multiple charts
+
+def load_stock(*tickers):
+    tickers = [t.strip().upper() for t in tickers]
+    data = yf.download(tickers, start = '2010-01-01', end = '2024-12-31')
+    data['Open'].plot(figsize= (10,5), title = f'{tickers} Opening prices from 2010 to 2024')
+    plt.grid(True)
+    plt.ylabel("Price in USD")
+    plt.xlabel("Year")
+    plt.show()
+
+    plt.figure(figsize = (10,6))
+    
+    plt.subplot(2,1,1)
+    plt.plot(data["High"])
+    plt.grid(True)
+    plt.ylabel('Price in USD')
+    plt.xlabel('Year')
+    plt.title(f' Prices at High for {tickers}')
+
+    plt.subplot(2,1,2)
+    plt.plot(data["Volume"])
+    plt.grid(True)
+    plt.ylabel('Volume of trade')
+    plt.xlabel('Year')
+    plt.title(f'Volume of trades for {tickers}')
+
+    plt.tight_layout()
+    plt.show()
+
+load_stock('MSFT', 'NDAQ')
+
