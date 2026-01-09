c9477563a04f66c46e12d0f83d364a49da1a8c2c
Ash-Y89 <ashe.yari@gmail.com>
2026-01-09 15:22:43 -0600

    refined to object-oriented approach

--- stocks/correlation_heatmaps.py
+++ stocks/correlation_heatmaps.py
@@ -7,8 +7,8 @@ def corr_heatmap(*tickers):
     data = yf.download(tickers, start = "2020-01-01", end = "2024-12-31")
     returns = data[["Open", "Close"]].pct_change()
     corr = returns.corr()
-    sns.heatmap(corr, annot = True, cmap= "coolwarm", linewidths=0.5)
-    plt.title(f"Correlation Heatmap: {tickers}")
+    c1 = sns.heatmap(corr, annot = True, cmap= "coolwarm", linewidths=0.5)
+    c1.set_title(f"Correlation Heatmap: {tickers}")
     plt.show()
 
 corr_heatmap("AAPL", "NDAQ")
